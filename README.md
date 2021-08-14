79d71239e371be520679bae2278c528f2466989c
unknown <michaeljamescarnaje1@gmail.com>
2021-08-14 15:12:13 +0800

    Pathfinding Visualizer: Next.js typescript INIT

---
+++ README.md
@@ -0,0 +1,1 @@
+My version of Pathfinding Visualizer tutorial by Clement
---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+          <title>Pathfinding Visualizer</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
